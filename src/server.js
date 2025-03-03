71751c8b24de75aa63839dcc70855511c9ebeae7
AquilezE <pochitaisking@gmail.com>
2025-03-03 16:47:53 -0600

    modifique el server y la variable de entorno

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 
@@ -8,6 +9,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+mongoose
+.connect(process.env.MONGO_URI, { useNewUrlParser: true,
+useUnifiedTopology: true })
+.then(() => console.log("MongoDB conectado"))
+.catch((err) => console.error("Error de conexión:", err));
+
+const photoRoutes = require("./routes/photoRoutes");
+app.use("/api/photos", photoRoutes);
+
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servicitio corriendo en puerto ${PORT}`));
 
 
